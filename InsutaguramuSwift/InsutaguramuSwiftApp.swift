f84e69a0984f15f899a94354e44b78e515751d9c
junsai <junsai0530@gmail.com>
2022-07-31 17:49:42 +0900

    Initial Commit

---
+++ InsutaguramuSwift/InsutaguramuSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsutaguramuSwiftApp.swift
+//  InsutaguramuSwift
+//
+//  Created by 小暮準才 on 2022/07/31.
+//
+
+import SwiftUI
+
+@main
+struct InsutaguramuSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
