c9675b8c502853afae7b2553bf4d77d2322c70d9
junsai <junsai0530@gmail.com>
2022-08-03 03:08:41 +0900

    upload view

--- InsutaguramuSwift/UploadPost.swift
+++ InsutaguramuSwift/UploadPost.swift
@@ -11,15 +11,18 @@ import Firebase
 
 
 class UploadPostViewModel: ObservableObject {
+    
+    
     @Published var errorMessage = "error"
     
     
-    func uploadPost(uploadText: String, currentUser: User) {
+    func uploadPost(uploadText: String, uploadImage:UIImage , currentUser: User, completion: @escaping (Bool)-> Void) {
         
         if uploadText.count < 5 {
             self.errorMessage = "more than 4 characters"
             return
         }
+        
         
         let uploadPostData = [
             "authorUid" : currentUser.uid,
@@ -33,16 +36,60 @@ class UploadPostViewModel: ObservableObject {
             "didLike": false,
         ] as [String:Any]
         
-        Firestore.firestore().collection("posts").document().setData(uploadPostData) { error in
+        var storeRef: DocumentReference?
+        storeRef = Firestore.firestore().collection("posts").addDocument(data: uploadPostData) { error in
             if let error = error {
                 print(error)
                 return
             }
-            
+            guard let docId = storeRef?.documentID else {
+                print("no doc id")
+                return }
+            let ref = Storage.storage().reference(withPath: "posts/\(docId)")
             self.errorMessage = "upload done"
+            
+            
+            guard let imageData = uploadImage.jpegData(compressionQuality: 0.5) else {
+                self.errorMessage = "no image"
+                return }
+            
+            
+            ref.putData(imageData, metadata: nil) { metadata, error in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                ref.downloadURL { URL, error in
+                    if let error = error {
+                        print(error)
+                        return
+                    }
+                    
+                    self.errorMessage = "\(URL?.absoluteString ?? "no url")"
+                    
+                    guard let uploadImageUrl = URL?.absoluteString else { return }
+                    
+                    
+                    Firestore.firestore().collection("posts").document(docId).updateData(["postImageUrl": uploadImageUrl]) { error in
+                        if let error = error {
+                            print(error)
+                            return
+                        }
+                        
+                        self.errorMessage = "store image done"
+                        completion(true)
+                    }
+                    
+                    
+                }
+                
+                
+            }
         }
         
     }
+    
+    
     
 }
 
@@ -52,6 +99,8 @@ struct UploadPost: View {
     
     @ObservedObject var vm = UploadPostViewModel()
     @State var uploadText: String = ""
+    @State var uploadImage: UIImage?
+    @State private var showImagePicker = false
     
     
     var body: some View {
@@ -69,8 +118,13 @@ struct UploadPost: View {
                     Spacer()
                     
                     Button {
-                        vm.uploadPost(uploadText: self.uploadText, currentUser: vmAuth.currentUser!)
-                        self.uploadText = ""
+                        vm.uploadPost(uploadText: self.uploadText, uploadImage: self.uploadImage! , currentUser: vmAuth.currentUser!){ didUpload in
+                            if didUpload == true {
+                                self.uploadImage = nil
+                                self.uploadText = ""
+                            }
+                        }
+                        
                     } label: {
                         Text("upload")
                             .foregroundColor(Color.white)
@@ -89,6 +143,7 @@ struct UploadPost: View {
                     TextEditor(text: $uploadText)
                         .padding()
                         .background(Color.gray)
+                        .frame(width: .infinity, height: 200)
                     
                     if self.uploadText.isEmpty {
                         
@@ -97,6 +152,7 @@ struct UploadPost: View {
                                 .foregroundColor(Color.gray)
                                 .padding(.horizontal)
                                 .padding(.vertical, 8)
+                            
                             Spacer()
                         }
                         .padding()
@@ -104,9 +160,33 @@ struct UploadPost: View {
                         
                     }
                 }
+                
+                Button {
+                    self.showImagePicker.toggle()
+                } label: {
+                    if let uploadImage = uploadImage {
+                        Image(uiImage: uploadImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: .infinity, height: 400, alignment: .center)
+                            .scaledToFit()
+                        
+                    } else {
+                        Image(systemName: "photo")
+                            .font(.system(size: 120))
+                            .foregroundColor(Color.black)
+                            .padding()
+                    }
+                }
+                
+                Spacer()
+                
             }
         }
-
+        .fullScreenCover(isPresented: $showImagePicker) {
+            ImagePicker(image: $uploadImage)
+        }
+        
     }
 }
 
