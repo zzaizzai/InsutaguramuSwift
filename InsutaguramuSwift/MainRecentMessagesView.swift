dead3add1144c7b5b8ebc3b31c2201247d7ae256
junsai <junsai0530@gmail.com>
2022-08-12 04:24:11 +0900

    recent messages bug fixed

--- InsutaguramuSwift/MainRecentMessagesView.swift
+++ InsutaguramuSwift/MainRecentMessagesView.swift
@@ -19,7 +19,7 @@ struct RecentMessage: Identifiable, Codable {
     
     var chatUser : User?
     
-    init(documentId: String, data: [String:Any]) {
+    init(documentId: String, data: [String:Any], user: User) {
         self.documentId = documentId
         self.uid = data["uid"] as? String ?? "no"
         self.email = data["email"] as? String ?? "no email"
@@ -27,6 +27,8 @@ struct RecentMessage: Identifiable, Codable {
         self.profileImageUrl = data["profileImageUrl"] as? String ?? "no profileImageUrl"
         self.recentText = data["recentText"] as? String ?? "no recentText"
         self.time = data["time"] as? Timestamp ?? Timestamp()
+        
+        self.chatUser = user
     }
 }
 
@@ -56,7 +58,7 @@ class MainRecentMessagesViewModel: ObservableObject {
             guard let documentId = snapshot?.documentID else { return }
             guard let data = snapshot?.data() else { return }
             
-        
+            
             
             self.myUser = User(documentId: documentId, data: data)
             
@@ -69,11 +71,11 @@ class MainRecentMessagesViewModel: ObservableObject {
     func fetchRecentMessages() {
         // get rm
         
-//        self.recentMessages.removeAll()
+        //        self.recentMessages.removeAll()
         
         guard let user = self.myUser else { return }
         
-        Firestore.firestore().collection("recentMessages").document(user.uid).collection("recentMessages").getDocuments { snapshots, error in
+        Firestore.firestore().collection("recentMessages").document(user.uid).collection("recentMessages").order(by: "time").getDocuments { snapshots, error in
             if let error = error {
                 print(error)
                 return
@@ -83,30 +85,29 @@ class MainRecentMessagesViewModel: ObservableObject {
                 let documentId = doc.documentID
                 let data = doc.data()
                 
-                self.recentMessages.insert(.init(documentId: documentId, data: data), at: 0)
-                
-                Firestore.firestore().collection("users").document(self.recentMessages[0].uid).getDocument { usersnapshot , error in
-                    if let error2 = error {
-                        print(error2)
-                        return
-                    }
+                self.getUserDataOfRecentMessages(chatUserUid: documentId) { userData in
+                    self.recentMessages.insert(.init(documentId: documentId, data: data, user: userData), at: 0)
                     
-                    
-                    guard let userId = usersnapshot?.documentID else { return }
-                    guard let userData = usersnapshot?.data() else { return }
-                    
-                    self.recentMessages[0].chatUser = User(documentId: userId, data: userData)
                 }
                 
             })
             
-            
-           
         }
     }
     
-    func fetchRecentMessagesProfile() {
-        // get user profiles
+    func getUserDataOfRecentMessages(chatUserUid: String, completion: @escaping (User)-> ()) {
+        Firestore.firestore().collection("users").document(chatUserUid).getDocument { snapshot, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let userId = snapshot?.documentID else { return}
+            guard let data = snapshot?.data() else { return }
+            
+            completion(User(documentId: userId, data: data))
+        }
+        
     }
     
 }
@@ -118,15 +119,13 @@ struct MainRecentMessagesView: View {
     @ObservedObject var vm = MainRecentMessagesViewModel()
     
     
-
-    
     var body: some View {
         ScrollView{
             LazyVStack(alignment: .leading){
                 
                 ForEach(vm.recentMessages){ recentMessage in
                     RecentMessageView(recentMessage: recentMessage)
-
+                    
                     
                 }
             }
@@ -145,8 +144,6 @@ struct MainRecentMessagesView: View {
 
 
 class RecentMessageViewModel: ObservableObject{
-    
-    
     
     
 }
@@ -181,6 +178,7 @@ struct RecentMessageView: View{
                 Text(recentMessage.name)
                     .fontWeight(.bold)
                 Text(recentMessage.recentText)
+                    .lineLimit(1)
                 
             }
             
